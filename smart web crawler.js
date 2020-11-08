ea917cd7c450cb2e108090a8b5ed8ab779f1a189
Shaukat456 <65353819+Shaukat456@users.noreply.github.com>
2020-11-08 22:14:07 +0500

    Update smart web crawler.js

--- smart web crawler.js
+++ smart web crawler.js
@@ -1,8 +1,9 @@
-let str='java'
+let str='java' // JAVA IS TAKEN AS AN EXAMPLE HERE
 let links=document.links;
 let href;
 //Main code 
 Array.from(links).forEach(function(element){
     href=element.href;
     console.log(href)
+    
 })
